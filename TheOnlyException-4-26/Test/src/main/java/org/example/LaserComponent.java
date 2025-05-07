0b0cd8a89e88d6e426d65ccc0052f1e9bdb880f9
Tips-y <152687869+Tips-y@users.noreply.github.com>
2025-05-07 16:43:00 +0800

    Update LaserComponent.java
    volt chain

--- TheOnlyException-4-26/Test/src/main/java/org/example/LaserComponent.java
+++ TheOnlyException-4-26/Test/src/main/java/org/example/LaserComponent.java
@@ -7,12 +7,21 @@ import java.util.Random;
 
 public class LaserComponent extends Component {
     private Point2D direction;
-    private final double speed = 5;
+    private final double speed = 20;
+    private int enemiesHit = 0;
     private int damage;
 
     public LaserComponent() {
         Random rand = new Random();
         this.damage = 25;
+    }
+
+    public void setEnemiesHit() {
+        enemiesHit++;
+        if (enemiesHit > 4) {
+            enemiesHit = 4;
+        }
+        setDamage();
     }
 
     public void setDirection(Point2D direction) {
@@ -22,9 +31,31 @@ public class LaserComponent extends Component {
     @Override
     public void onUpdate(double tpf) {
         entity.translate(direction);
+        updateOpacity(); // Call method to update opacity
     }
 
     public int getDamage() {
         return damage;
+    }
+
+    public void setDamage() {
+        this.damage -= 5 * enemiesHit;
+        if (damage < 5) {
+            damage = 5;
+        }
+    }
+
+    private void updateOpacity() {
+        double opacity = 0.75; // Default
+        if (damage == 20) {
+            opacity = 0.75;
+        } else if (damage == 15) {
+            opacity = 0.50;
+        } else if (damage == 13) {
+            opacity = 0.35;
+        } else if (damage <= 5) { // Changed to <= to include 5 and below
+            opacity = 0.20;
+        }
+        entity.setOpacity(opacity); // Set the opacity of the entity.
     }
 }
