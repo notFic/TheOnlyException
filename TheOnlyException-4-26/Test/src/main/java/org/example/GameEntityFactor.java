16fa9e130eb0162acc61e8feeaba5c006ff0694e
Tips-y <152687869+Tips-y@users.noreply.github.com>
2025-05-09 02:21:15 +0800

    Update GameEntityFactor.java

--- TheOnlyException-4-26/Test/src/main/java/org/example/GameEntityFactor.java
+++ TheOnlyException-4-26/Test/src/main/java/org/example/GameEntityFactor.java
@@ -3,6 +3,7 @@ package org.example;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.components.ExpireCleanComponent;
 import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
